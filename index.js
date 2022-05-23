e25f83734a5522eaea7b3b7783634572dc41aab8
Vasko Nikolov <hugovasko1@gmail.com>
2022-05-23 11:49:35 +0300

    Some part was restructured, JavaScript was fixed and now website is fully working

--- index.js
+++ index.js
@@ -6,36 +6,28 @@ imageArray[3] = "images/dice4.png";
 imageArray[4] = "images/dice5.png";
 imageArray[5] = "images/dice6.png";
 
-function firstRandomNumber(){
-    let randomNumber1 = Math.floor(Math.random()*5);
-    return randomNumber1;
-}
-
-function secondRandomNumber(){
-    let randomNumber2 = Math.floor(Math.random()*5);
-    return randomNumber2;
-}
 
 function changePic(){
-    document.querySelector(".img1").src = imageArray[firstRandomNumber()];
-    document.querySelector(".img2").src = imageArray[secondRandomNumber()];
+
+    let randomNumber1 = Math.floor(Math.random()*5);
+    let randomNumber2 = Math.floor(Math.random()*5);
+
+    document.querySelector(".img1").src = imageArray[randomNumber1];
+    document.querySelector(".img2").src = imageArray[randomNumber2];
+
+    if(randomNumber1 > randomNumber2){
+        document.querySelector(".title").innerHTML = "<span>&#128681</span>Player 1 Wins!";
+    }
+    else if(randomNumber1 < randomNumber2){
+        document.querySelector(".title").innerHTML = "Player 2 Wins!<span>&#128681</span>";
+    }
+    else {
+        document.querySelector(".title").innerHTML = "Draw!";
+    }
+
 }
 
 changePic();
-
-function changeTitle(){
-    if(firstRandomNumber() > secondRandomNumber()){
-        document.querySelector(".title").innerHTML = "Player 1 Wins";
-    }
-    else if(firstRandomNumber() < secondRandomNumber()){
-        document.querySelector(".title").innerHTML = "Player 2 Wins";
-    }
-    else if(firstRandomNumber() === secondRandomNumber()){
-        document.querySelector(".title").innerHTML = "Draw";
-    }
-}
-
-changeTitle();
 
 
 
